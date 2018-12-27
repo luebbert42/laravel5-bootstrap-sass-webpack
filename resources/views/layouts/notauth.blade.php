7ac212c6ba3185ac941461de631f5e8c706e8c2d
Dorthe Luebbert <dorthe@luebbert.net>
2018-12-27 19:13:52 +0100

    livereload removed

--- resources/views/layouts/notauth.blade.php
+++ resources/views/layouts/notauth.blade.php
@@ -14,10 +14,5 @@
 
 <script src="{{asset('js/app.js')}}?v={{$version}}"></script>
 @yield('js')
-@if ( Config::get('app.debug') )
-    <script type="text/javascript">
-        document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
-    </script>
-@endif
 </body>
 </html>
