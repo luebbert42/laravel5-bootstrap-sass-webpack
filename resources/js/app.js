e1a3084e7ff6a07921d61fc3c809c19264682850
Dorthe Luebbert <dorthe@luebbert.net>
2018-12-27 21:01:57 +0100

    app adjusted

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,18 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('bootstrap');
-
 window.Vue = require('vue');
+
+import Axios from 'axios';
+import Datepicker from 'vuejs-datepicker';
+import VuejsDialog from "vuejs-dialog";
+import VueCookie from "vue-cookie";
+
+Vue.prototype.$http = Axios;
+Vue.use(VuejsDialog);
+Vue.use(VueCookie);
+
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +24,32 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('resetform', require('./components/ResetForm.vue').default);
+
+export const eventBus = new Vue();
+
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    mixins: Laravel.vueMixins,
+    data: {
+        msg: false,
+    },
+    components: {
+        Datepicker
+    },
+    created() {
+    },
+    methods: {
+        jserror: function(msg) {
+            switch (msg) {
+                case "defaultMsg500":
+                    this.msg = "Ein Fehler ist aufgetreten. Bitte versuchen Sie es in wenigen Minuten erneut.";
+                    this.msg += "Falls diese Fehlermeldung erneut auftritt, kontaktieren Sie bitte den Support."
+                    break;
+                default:
+                    this.msg = " Ein Fehler ist aufgetreten. ";
+            }
+        }
+    }
 });
---
+++ resources/js/components/ResetForm.vue
@@ -0,0 +1,35 @@
+<template>
+       <span class="m-r-large">
+                  <button class="btn btn-sm btn-default" type="reset" @click="resetForm" >Zurücksetzen</button>
+       </span>
+</template>
+
+<script>
+    export default {
+        props: ['formname'],
+        data () {
+            return {
+            }
+        },
+        methods: {
+            resetForm: function(e) {
+                let elements = document.forms[this.formname].getElementsByTagName("input");
+                let i;
+                for (i = 0; i < elements.length; i++) {
+                    if (elements[i].type == "checkbox") {
+                        elements[i].checked = false;
+                    }
+                    else if (elements[i].type == "text") {
+                        elements[i].value = "";
+                    }
+                }
+                elements = document.forms[this.formname].getElementsByTagName("select");
+                for (i = 0; i < elements.length; i++) {
+                    elements[i].selectedIndex = null;
+                }
+                event.preventDefault();
+            }
+        },
+    }
+</script>
+
