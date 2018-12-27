53bb812380ffff591278cf7d323577647586ed0e
Dorthe Luebbert <dorthe@luebbert.net>
2018-12-27 20:32:20 +0100

    breadcrumb bootstrap 4

--- resources/views/layouts/partials/_breadcrumb.blade.php
+++ resources/views/layouts/partials/_breadcrumb.blade.php
@@ -1,18 +1,20 @@
     <!-- .breadcrumb -->
-    <ul class="breadcrumb">
-        <li><a href="{{route("home")}}"><i class="fa fa-home"></i> Home</a></li>
-        @if(isset($breadcrumbs) && count($breadcrumbs) > 0)
-            @foreach($breadcrumbs as $route => $label)
-                @if ($label == end($breadcrumbs))
-                    <li class="active" >
-                        {{$label}}
-                    </li>
-                 @else
-                    <li >
-                        <a href="{{$route}}">{{$label}}</a>
-                    </li>
-                @endif
-            @endforeach
-        @endif
-    </ul>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route("home")}}"><i class="fa fa-home"></i> Home</a></li>
+            @if(isset($breadcrumbs) && count($breadcrumbs) > 0)
+                @foreach($breadcrumbs as $route => $label)
+                    @if ($label == end($breadcrumbs))
+                        <li class="active breadcrumb-item" aria-current="page">
+                            {{$label}}
+                        </li>
+                    @else
+                        <li class="breadcrumb-item">
+                            <a href="{{$route}}">{{$label}}</a>
+                        </li>
+                    @endif
+                @endforeach
+            @endif
+        </ol>
+    </nav>
     <!-- / .breadcrumb -->
